e129f01f6f1d61c8f1a0300c305736232c4b104b
Schwimmer <aschwimm@admin.touro.edu>
2022-02-02 20:00:22 -0500

    Added QuadraticEquation code

---
+++ src/main/java/ProjectileMotion.java
@@ -0,0 +1,10 @@
+public class ProjectileMotion {
+
+    public static void main(String[] args) {
+        Projectile p = new Projectile(52, 43);
+
+        System.out.println(p.getX(7));
+        System.out.println(p.getY(7));
+    }
+
+}
---
+++ src/main/java/QuadraticEquation.java
@@ -0,0 +1,9 @@
+public class QuadraticEquation {
+
+
+
+    public double[] getX() {
+
+    }
+
+}
