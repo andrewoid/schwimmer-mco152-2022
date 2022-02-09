3edd7f9ab197051e138eb31bcec9a78d3104e0b2
Schwimmer <aschwimm@admin.touro.edu>
2022-02-09 17:30:34 -0500

    added QuadraticEquation

--- src/main/java/QuadraticEquation.java
+++ src/main/java/QuadraticEquation.java
@@ -1,9 +1,20 @@
 public class QuadraticEquation {
 
+    private final double a;
+    private final double b;
+    private final double c;
 
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
 
     public double[] getX() {
-
+        return new double[] {
+                (-b + Math.sqrt(b*b - 4*a*c)) / 2*a,
+                (-b - Math.sqrt(b*b - 4*a*c)) / 2*a
+        };
     }
 
 }
