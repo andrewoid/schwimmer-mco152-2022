19882e374de01f4c7407add9f5d13368c701c25c
Andrew Schwimmer <andrew.schwimmer@disneystreaming.com>
2022-04-06 08:56:16 -0400

    checked position of curly brances to see if it fails checkstyles

--- src/main/java/LetterPool.java
+++ src/main/java/LetterPool.java
@@ -4,7 +4,8 @@ public class LetterPool {
 
     private Random random = new Random();
 
-    public char getRandomLetter() {
+    public char getRandomLetter()
+    {
         return (char) ('A' + random.nextInt(26));
     }
 
