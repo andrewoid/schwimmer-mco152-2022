239b948ad11acebb835a2113a9b6dd35e6dd9851
Andrew Schwimmer <andrew.schwimmer@disneystreaming.com>
2022-05-11 19:32:32 -0400

    added animation

--- src/main/java/weather/TemperatureSign.java
+++ src/main/java/weather/TemperatureSign.java
@@ -4,18 +4,24 @@ import javax.swing.JComponent;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class TemperatureSign extends JComponent {
 
     private double temperature;
+    private static final int CAR_WIDTH = 20;
+    private static final int CAR_HEIGHT = 60;
+    private double angle = 0;
 
     public TemperatureSign() {
         setPreferredSize(new Dimension(400,400));
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+    protected void paintComponent(Graphics g1) {
+        super.paintComponent(g1);
+
+        Graphics2D g = (Graphics2D) g1;
 
         if (temperature > 50) {
             g.setColor(Color.RED);
@@ -23,8 +29,28 @@ public class TemperatureSign extends JComponent {
         else {
             g.setColor(Color.BLUE);
         }
-        g.drawLine(0,0, 400, 400);
-        g.drawLine(0,400, 400, 0);
+
+        int width = getWidth();
+        int height = getHeight();
+
+        g.drawLine(0,0, width, height);
+        g.drawLine(0, height, width, 0);
+
+        g.drawOval(0, 0, width, height);
+
+        g.setColor(Color.RED);
+
+        // this moves the origin
+        g.translate(width / 2, height / 2);
+        g.rotate(Math.toRadians(angle));
+        angle += 0.1;
+
+        g.fillRect(-CAR_WIDTH / 2,
+                -CAR_HEIGHT / 2,
+                CAR_WIDTH,
+                CAR_HEIGHT);
+
+        repaint();
     }
 
     public void setTemperature(double temperature) {
